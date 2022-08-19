4842703f873911231487584e61dedc9461d994c7
vanfossen <nvanfossen@gmail.com>
2022-08-18 21:33:41 -0400

    background sample working!!

--- flask/app.py
+++ flask/app.py
@@ -23,31 +23,38 @@ def ftir():
 
 @app.route("/fetch_background", methods=["POST"])
 def fetch_background():
+    # put incoming JSON into a dictionary
     data = json.loads(request.data)
-    # Need to add Param Check
-    print(
-        calc_spectrum(
-            data["minWave"],
-            data["maxWave"],
-            molecule=None,
-            # isotope="1,2,3",
-            # pressure=data["pressure"],
-            Tgas=294.15,  # hardcode
-            path_length=10,  # hardcode
-            wstep=0.5,  # (cm^-1)
-            verbose=False,  # hides HITRAN output
-            databank="hitran",
-            warnings={"AccuracyError": "ignore"},
-        )
-    )
-    result = ""
 
-    # convert dictionary values to strings and return as JSON
-    return {
-        "success": True,
-        "x": list(result.keys()),
-        "y": [str(flt) for flt in result.values()],
-    }
+    print(data)
+
+    # verify the information in the dictionary
+    __param_check(data)
+
+    print("----- output verified params to console as self-check -----")
+    for key, value in data.items():
+        print("  %s: %s" % (key, value))
+
+    # perform: transmission spectrum of gas sample (calc_spectrum)
+    #      --> blackbody spectrum of source (sPlanck)
+    #      --> transmission spectrum of beamsplitter and cell windows
+    #      --> detector response spectrum
+    print("----- start __generate_background() -----")
+    result = __generate_background(data)
+    print("----- end __generate_background() -----")
+
+    if result != False:
+        # convert dictionary values to strings and return as JSON
+        return {
+            "success": True,
+            "x": list(result.keys()),
+            "y": [str(flt) for flt in result.values()],
+        }
+    else:
+        return {
+            "success": False,
+            "text": "No line in the specified wavenumber range",
+        }
 
 
 @app.route("/post_json", methods=["POST"])
@@ -213,6 +220,10 @@ def __MCT(data):
         / (2 * np.sqrt(np.pi / (4 * np.log(2))))
         * np.exp(-4 * np.log(2) * ((x_um - 18.6) ** 2) / (2**2))
     )
+
+
+def __zeroY(data):
+    return (data * 0) + 1
 
 
 def __sPlanck(spectrum, temp):
@@ -481,14 +492,181 @@ def __generate_spectra(data):
 
         spectrum = multiply(spectrum, factor, var="transmittance_noslit")
 
-
     # NOTE: Hardcoded for now, might? need user parameters and likely its own function
     find_peaks = spectrum.to_specutils()
-    noise_region = SpectralRegion((1 / data["minWave"]) / u.cm, (1 / data["maxWave"]) / u.cm)
+    noise_region = SpectralRegion(
+        (1 / data["minWave"]) / u.cm, (1 / data["maxWave"]) / u.cm
+    )
     find_peaks = noise_region_uncertainty(find_peaks, noise_region)
     lines = find_lines_threshold(find_peaks, noise_factor=6)
     print()
     print(lines)
+
+    return __loadData(spectrum.get("transmittance_noslit", wunit="nm", Iunit="default"))
+
+
+def __generate_background(data):
+    try:
+        wstep = __calc_wstep(data["resolution"], data["zeroFill"])
+        # ----- a.) transmission spectrum of gas sample -----
+        # https://radis.readthedocs.io/en/latest/source/radis.lbl.calc.html#radis.lbl.calc.calc_spectrum
+        s = calc_spectrum(
+            data["minWave"],
+            data["maxWave"],
+            molecule=data["molecule"],
+            isotope="1,2,3",
+            pressure=data["pressure"],
+            Tgas=294.15,  # hardcode
+            path_length=10,  # hardcode
+            wstep=wstep,  # (cm^-1)
+            verbose=False,  # hides HITRAN output
+            databank="hitran",
+            warnings={"AccuracyError": "ignore"},
+        )
+    except:
+        return False
+
+    # spectrum = __loadData(s.get("transmittance_noslit", wunit="nm", Iunit="default"))
+    w = s.get_wavelength()
+    # ----- b.) blackbody spectrum of source -----
+    spec_zeroY = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __zeroY(w),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+    spec_sPlanck = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __sPlanck(w, data["source"]),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+    spec_AR_ZnSe = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __AR_ZnSe(w),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+    spec_AR_CaF2 = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __AR_CaF2(w),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+    spec_CaF2 = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __CaF2(w),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+    spec_ZnSe = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __ZnSe(w),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+    spec_sapphire = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __sapphire(w),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+    spec_MCT = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __MCT(w),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+    spec_InSb = Spectrum(
+        {
+            "wavelength": w,
+            "transmittance_noslit": __InSb(w),
+            "radiance_noslit": np.zeros_like(w),
+        },
+        wunit="nm",
+        units={"radiance_noslit": "mW/cm2/sr/nm", "transmittance_noslit": ""},
+        name="CaF2 window",
+    )
+
+    print("before")
+    spectrum = SerialSlabs(spec_zeroY, spec_sPlanck)
+    print("after")
+
+    # ----- c.) transmission spectrum of windows/beamsplitter -----
+    for x in range(data["numScan"]):
+        print(x)
+
+        # Beamsplitter
+        if data["beamsplitter"] == "AR_ZnSe":
+            spectrum = SerialSlabs(spectrum, spec_AR_ZnSe)
+        elif data["beamsplitter"] == "AR_CaF2":
+            spectrum = SerialSlabs(spectrum, spec_AR_CaF2)
+
+        # Cell Windows
+        if data["cellWindow"] == "CaF2":
+            spectrum = SerialSlabs(spectrum, spec_CaF2)
+            spectrum = SerialSlabs(spectrum, spec_CaF2)
+        elif data["cellWindow"] == "ZnSe":
+            spectrum = SerialSlabs(spectrum, spec_ZnSe)
+            spectrum = SerialSlabs(spectrum, spec_ZnSe)
+
+        # ----- d.) detector response spectrum -----
+        if data["detector"] == "MCT":
+            spectrum = SerialSlabs(spectrum, spec_ZnSe)
+            spec_MCT = add_array(
+                spec_MCT,
+                np.random.normal(0, 20000000, len(spec_MCT.get_wavelength())),
+                var="transmittance_noslit",
+            )
+            spectrum = SerialSlabs(spectrum, spec_MCT)
+        elif data["detector"] == "InSb":
+            spectrum = SerialSlabs(spectrum, spec_sapphire)
+            spec_InSb = add_array(
+                spec_InSb,
+                np.random.normal(0, 200000000, len(spec_MCT.get_wavelength())),
+                var="transmittance_noslit",
+            )
+            spectrum = SerialSlabs(spectrum, spec_InSb)
+
+        # Normalize
+        numbers = __loadData(
+            spectrum.get("transmittance_noslit", wunit="nm", Iunit="default")
+        )
+        factor = 1 / sum(numbers.values())
+
+        spectrum = multiply(spectrum, factor, var="transmittance_noslit")
 
     return __loadData(spectrum.get("transmittance_noslit", wunit="nm", Iunit="default"))
 
